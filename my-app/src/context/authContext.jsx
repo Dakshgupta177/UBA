1f239c0d09c91e4c780744703fe9721ea552578b
4bluee <sansitasingh5@gmail.com>
2026-02-11 10:34:49 +0530

    minor fix

--- my-app/src/context/authContext.jsx
+++ my-app/src/context/authContext.jsx
@@ -3,7 +3,7 @@ import API from '../api/axios';
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
+export function AuthProvider ({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
